dbc43e133182e33e7f285fd80f62740acab49ab7
Josh McLain <joshmclain45@gmail.com>
2022-04-18 18:15:05 -0700

    added two ejs pages

--- index.mjs
+++ index.mjs
@@ -1,8 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => res.render('home'));
+app.get('/about', (req, res) => res.render('about'));
 
 app.listen(PORT, () => console.log(`webapp running on port ${PORT}`));

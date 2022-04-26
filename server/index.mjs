5506b7498dd92db1ddb744bc1603eb5fead50f87
Josh McLain <joshmclain45@gmail.com>
2022-04-25 18:59:08 -0700

    refator naming, fix eslint

--- server/index.mjs
+++ server/index.mjs
@@ -5,8 +5,9 @@ const router = express.Router();
 router.get('/', (req, res) => res.render('home'));
 router.get('/about', (req, res) => res.render('about'));
 router.get('/apply', (req, res) => res.render('apply'));
-router.get('/register', (req, res) => res.render('register'));
 router.get('/contact', (req, res) => res.render('contact'));
+
+router.get('/login', (req, res) => res.render('login'));
 router.get('/dashboard', (req, res) => res.render('dashboard'));
 
 export default router;

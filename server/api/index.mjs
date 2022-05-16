9727cb5e4bb20c4bcb42a2a73f43bd49df5e0983
Josh McLain <joshmclain45@gmail.com>
2022-05-15 23:33:02 -0700

    add post method for patient

--- server/api/index.mjs
+++ server/api/index.mjs
@@ -1,10 +1,69 @@
 import express from 'express';
-// import console from 'console';
+import PatientModel from '../../models/Patient.mjs';
 
 const router = express.Router();
 
-router.route('/client').post((request, response) => {
-  return response.redirect('dashboard', { user: request.user });
-});
+router
+  .route('/patient')
+  .post((request, response) => {
+    const patientList = PatientModel.find();
+    const {
+      firstName,
+      lastName,
+      ahcccsId,
+      locationName,
+      locationAdress,
+      phoneNumber,
+      prefferedDriver,
+    } = request;
+    if (
+      !firstName ||
+      !lastName ||
+      !ahcccsId ||
+      !locationName ||
+      !locationAdress ||
+      !phoneNumber ||
+      !prefferedDriver
+    ) {
+      return response.render('dashboard', {
+        user: request.user,
+        error: 'Error with request',
+        patientList,
+      });
+    } else {
+      const isPresent = PatientModel.find({ ahcccsId });
+      if (isPresent) {
+        return response.render('dashboard', {
+          user: request.user,
+          error: 'Patient already in system',
+          patientList,
+        });
+      } else {
+        const newPatient = new PatientModel({
+          firstName,
+          lastName,
+          ahcccsId,
+          locationName,
+          locationAdress,
+          phoneNumber,
+          prefferedDriver,
+        });
+        PatientModel.bulkSave(newPatient);
+        const newPatientList = PatientModel.find();
+        return response.render('dashboard', {
+          user: request.user,
+          patientList: newPatientList,
+        });
+      }
+    }
+  })
+  .put((request, response) => {
+    const patientList = PatientModel.find();
+    return response.render('dashboard', {
+      user: request.user,
+      error: 'Error with request',
+      patientList,
+    });
+  });
 
 export default router;
